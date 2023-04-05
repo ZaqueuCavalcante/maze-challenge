6504e623e0eb80b35aaf757216233096ef9173d8
ZaqueuCavalcante <zaqueudovale@gmail.com>
2023-04-05 17:18:51 -0300

    Remove 01 things

--- src/test/java/stone/MazeChallenge03SolutionsTests.java
+++ src/test/java/stone/MazeChallenge03SolutionsTests.java
@@ -11,10 +11,10 @@ import org.junit.Test;
 
 import processing.core.PApplet;
 
-public class MazeChallenge01SolutionsTests {
-    @Test
-    public void should_test_many_correct_solutions_for_maze_challenge_01() {
-        File file = new File("src/test/java/stone/solutions/solutions_maze_challenge_01.txt");
+public class MazeChallenge03SolutionsTests {
+    // @Test
+    public void should_test_many_correct_solutions_for_maze_challenge_03() {
+        File file = new File("src/test/java/stone/solutions/solutions_maze_challenge_03.txt");
         InputStream input;
         String[] paths = new String[0];
 
@@ -27,7 +27,7 @@ public class MazeChallenge01SolutionsTests {
 
         for (String path : paths) {
             // Arrange
-            Maze maze = new MazeChallenge01();
+            Maze maze = new MazeChallenge03();
 
             // Act
             boolean isSolution = maze.isSolution(path);
